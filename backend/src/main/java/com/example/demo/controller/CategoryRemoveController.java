1764cf15ce64a18303e153158e08d8f86e4737e3
kakedashie1 <126971420+kakedashie1@users.noreply.github.com>
2025-10-26 21:34:20 +0900

    カテゴリー削除機能実装

--- backend/src/main/java/com/example/demo/controller/CategoryRemoveController.java
+++ backend/src/main/java/com/example/demo/controller/CategoryRemoveController.java
@@ -1,8 +1,44 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.demo.entity.Category;
+import com.example.demo.entity.LogDetail;
+import com.example.demo.form.CategoryRemoveForm;
+import com.example.demo.service.CategoryService;
+import com.example.demo.service.TimeService;
+
+import lombok.RequiredArgsConstructor;
 
 @Controller
+@RequiredArgsConstructor
 public class CategoryRemoveController {
 
+	private final CategoryService categoryService;
+	
+	private final TimeService timeService;
+
+	/*--- タスク削除リクエスト（タスク詳細画面より） ---*/
+	@PostMapping("/category-remove")
+	public String remove(
+			@ModelAttribute CategoryRemoveForm form,
+			Model model) {
+		
+		categoryService.remove(form.getCategoryId());
+		// タスク削除確認画面に遷移する
+		List<Category> list = categoryService.findAll();
+
+		LogDetail logDetail = timeService.findDetailByLogId(form.getLogId());
+
+		model.addAttribute("logDetail", logDetail);
+
+		model.addAttribute("categoryList", list);
+
+		return "category";
+	}
 }
--- backend/src/main/java/com/example/demo/form/CategoryRemoveForm.java
+++ backend/src/main/java/com/example/demo/form/CategoryRemoveForm.java
@@ -1,11 +1,11 @@
 package com.example.demo.form;
 
 import lombok.Data;
-@Data
-public class CategoryDeleteForm {
 
-	private Integer categorId;
-	
+@Data
+public class CategoryRemoveForm {
+
+	private Integer logId;
+	private Integer categoryId;
 	private String categoryName;
-	
 }
