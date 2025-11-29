4df8a23859a0fde244bc2009be158e85b4fdfc5f
kakedashie1 <126971420+kakedashie1@users.noreply.github.com>
2025-11-30 07:41:59 +0900

    モーダルウィンドウ開閉

--- backend/src/main/resources/static/js/time-log.js
+++ backend/src/main/resources/static/js/time-log.js
@@ -2,6 +2,8 @@
 
 
 {
+	
+//	ユーザログイン
 	const open = document.getElementById('open');
 	const list = document.querySelector('.list');
 	const close = document.getElementById('close');
@@ -19,4 +21,39 @@
 			open.classList.remove('hide');
 
 		});
+		
+//
+
+
+//ログ登録処理
+
+
+	const modal = document.getElementById('modal');
+	const mask = document.getElementById('mask');
+	const timeBtn = document.getElementById('time-btn');
+	const closeBtn = document.getElementById('close-btn');
+	const backBtn = document.getElementById('back');
+	const registBtn = document.getElementById('regist-btn');
+	const registTitle = document.getElementById('regist-title');
+	
+	registBtn.addEventListener('click', (e) => {
+		e.preventDefault();
+		modal.classList.remove('hidden');
+		mask.classList.remove('hidden');
+	});
+	
+	closeBtn.addEventListener('click', () => {
+		
+		modal.classList.add('hidden');
+		mask.classList.add('hidden');
+	});
+	
+	
+	
+	mask.addEventListener('click', () => {
+		
+		closeBtn.click();
+	});
+	
+
 }
